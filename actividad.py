6dd2488b99f73a31a27687d1e96f06a6759c3990
Obscurity96 <159748221+Obscurity96@users.noreply.github.com>
2024-03-08 05:51:53 -0700

    Add files via upload

---
+++ actividad.py
@@ -0,0 +1,5 @@
+# Actividad_Python
+
+print("¡Bienvenidos compañeros!")
+nombre = input("Por favor, ingresa tu nombre: ")
+print("Hola, " + nombre + "! Bienvenido de nuevo, que tengas un excelente turno.")
